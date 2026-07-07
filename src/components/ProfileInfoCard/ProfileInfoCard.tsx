3113afa1cc62ea53b821df96894ebd1ee16aa01a
Abhinanda M S <abhinandamadhurisathyan@gmail.com>
2026-07-07 10:10:04 +0530

    disabled editing for designation

--- src/components/ProfileInfoCard/ProfileInfoCard.tsx
+++ src/components/ProfileInfoCard/ProfileInfoCard.tsx
@@ -105,7 +105,7 @@ export default function ProfileInfoCard({
           <InputField
             label="Designation"
             value={profile.designation}
-            disabled={!editing}
+            disabled={true}
             onChange={(e) =>
               setProfile({
                 ...profile,
